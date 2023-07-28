ee0caa7a974655490dbe0469195d1f975ef7031c
Osvaldo Guzman <2505202015@mail.utec.edu.sv>
2023-07-27 18:29:17 -0600

    11 cambio total

---
+++ TiendaOnline/static/js/efectos.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.dropdown').hover(
+        function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideDown(150);
+        },
+        function() {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideUp(100);
+        }
+    );
+});
